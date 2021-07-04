784407bfcecdab69b32c53df022f4a3a73be0188
Paiva <danpaiva@live.com>
2021-07-04 14:30:32 -0300

    add Singleton

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -6,7 +6,20 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\tPadrão Singleton\n");
+
+            Console.WriteLine("Tentando criar uma instância s1...");
+            Singleton s1 = Singleton.Instance;
+
+            Console.WriteLine("Tentando criar uma instância s2...");
+            Singleton s2 = Singleton.Instance;
+
+            if (s1 == s2)
+                Console.WriteLine("Existe somente uma instância.");
+            else
+                Console.WriteLine("Existem instâncias diferentes.");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private int numberOfInstances = 0;
+
+        private Singleton()
+        {
+            Console.WriteLine("Instanciando dentro do construtor privado.\n");
+            numberOfInstances++;
+            Console.WriteLine($"Número de Instâncias: {numberOfInstances}\n");
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    Console.WriteLine($"Criando a primeira instância.\n");
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+    }
+}
